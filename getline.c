0481bd295d60cacc10ad3118b437e39fb1d2534d
youssef-abjaou <130617788+youssef-abjaou@users.noreply.github.com>
2023-08-23 23:14:52 +0100

    initial commit

--- getline.c
+++ getline.c
@@ -8,7 +8,7 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
  * _realloc - Reallocates a memory block using malloc and free.
  * @ptr: A pointer to the memory previously allocated.
  * @old_size: The size in bytes of the allocated space for ptr.
- * @new_size: The size in bytes for the new memory block.
+ * @new_size: The  size in bytes for the new memory block.
  * Return: If new_size == old_size - ptr.
  *         If new_size == 0 and ptr is not NULL - NULL.
  *         Otherwise - a pointer to the reallocated memory block.
